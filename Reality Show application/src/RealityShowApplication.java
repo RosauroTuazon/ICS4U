164f748a29b75ce1c5a9b4c5b7f6beba0a280805
Rosauro Tuazon <manaphy2008@live.com>
2015-09-29 13:39:27 -0400

    Still confused on what to do.

--- Reality Show application/src/RealityShowApplication.java
+++ Reality Show application/src/RealityShowApplication.java
@@ -14,10 +14,19 @@ public class RealityShowApplication {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean flag = false;
+		ContestantInformation contestant1 = new ContestantInformation();
+		do {
+			flag = false;
+		try
+		{
+			contestant1.setfirstname();
+			contestant1.setPostalCode();
+		}
+		}
 	    Scanner sc = new Scanner(System.in);
 	    String frame = sc.nextLine();
 	    System.out.println("Hello! Please enter your first name.");
-	    contestant1.setFirstName(frame);
 	}
 
 }
