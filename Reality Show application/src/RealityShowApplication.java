e6bd5f864250a57f6153566e51d401a207b7cb98
Rosauro Tuazon <manaphy2008@live.com>
2015-11-10 12:43:19 -0400

    Added the save feature(Yesterday/Monday)

--- Reality Show application/src/RealityShowApplication.java
+++ Reality Show application/src/RealityShowApplication.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
